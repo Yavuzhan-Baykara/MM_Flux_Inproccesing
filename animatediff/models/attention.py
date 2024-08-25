5f3e928c5c6215057d4c9c8127ca8006443d0620
Yavuzhan-Baykara <109503908+Yavuzhan-Baykara@users.noreply.github.com>
2024-08-25 18:14:13 +0300

    Update attention.py

--- animatediff/models/attention.py
+++ animatediff/models/attention.py
@@ -1,7 +1,7 @@
 # Adapted from https://github.com/huggingface/diffusers/blob/main/src/diffusers/models/attention.py
 
 from dataclasses import dataclass
-from typing import Optional
+from typing import Callable, Optional
 
 import torch
 import torch.nn.functional as F
