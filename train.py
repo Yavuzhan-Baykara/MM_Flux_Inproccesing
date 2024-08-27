114371b0908167f46a0690c3708f737f8a488f80
Yavuzhan-Baykara <109503908+Yavuzhan-Baykara@users.noreply.github.com>
2024-08-27 17:35:56 +0300

    Update train.py

--- train.py
+++ train.py
@@ -357,6 +357,7 @@ for name, layer in unet.named_modules():
             with torch.no_grad():
                 if not image_finetune:
                     pixel_values = rearrange(pixel_values, "b f c h w -> (b f) c h w")
+                    print("pixel_values" + str(pixel_values.shape)
                     latents = vae.encode(pixel_values).latent_dist
                     latents = latents.sample()
                     latents = rearrange(latents, "(b f) c h w -> b c f h w", f=video_length)
