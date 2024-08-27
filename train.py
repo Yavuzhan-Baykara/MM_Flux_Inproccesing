e0a22989b8ce029e8bb33434673661a9676af510
Yavuzhan-Baykara <109503908+Yavuzhan-Baykara@users.noreply.github.com>
2024-08-27 16:52:35 +0300

    Update train.py

--- train.py
+++ train.py
@@ -321,9 +321,6 @@ def main(
         unet.train()
         
         for step, batch in enumerate(train_dataloader):
-            print("batch:" + str(batch))
-            print("step:" + str(step))
-            
             if cfg_random_null_text:
                 batch['text'] = [name if random.random() > cfg_random_null_text_ratio else "" for name in batch['text']]
                 
