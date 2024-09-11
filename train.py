0509437927defd54d6a6c64a647636d4eb6e4125
Yavuzhan-Baykara <109503908+Yavuzhan-Baykara@users.noreply.github.com>
2024-09-11 17:41:10 +0300

    Update train.py

--- train.py
+++ train.py
@@ -410,6 +410,7 @@ def main(
                 print("noisy_latents.shape:", str(noisy_latents.shape))
                 print("timesteps.shape:",str(timesteps.shape))
                 print("encoder_hidden_states.shape:", str(encoder_hidden_states.shape))
+                encoder_hidden_states = torch.nn.Linear(768, 128).to(encoder_hidden_states.device)(encoder_hidden_states)
                 model_pred = unet(noisy_latents, timesteps, encoder_hidden_states).sample
                 
                 loss = F.mse_loss(model_pred.float(), target.float(), reduction="mean")
