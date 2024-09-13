8b598b2b16cd2dddae3fd8591ff0052dcd6c1610
Yavuzhan-Baykara <109503908+Yavuzhan-Baykara@users.noreply.github.com>
2024-09-13 15:17:57 +0300

    Update motion_module.py

--- animatediff/models/motion_module.py
+++ animatediff/models/motion_module.py
@@ -58,7 +58,7 @@ class VanillaTemporalModule(nn.Module):
         attention_block_types              =( "Temporal_Self", "Temporal_Self" ),
         cross_frame_attention_mode         = None,
         temporal_position_encoding         = False,
-        temporal_position_encoding_max_len = 24,
+        temporal_position_encoding_max_len = 5000,
         temporal_attention_dim_div         = 1,
         zero_initialize                    = True,
     ):
@@ -104,7 +104,7 @@ class TemporalTransformer3DModel(nn.Module):
         
         cross_frame_attention_mode         = None,
         temporal_position_encoding         = False,
-        temporal_position_encoding_max_len = 24,
+        temporal_position_encoding_max_len = 5000,
     ):
         super().__init__()
 
@@ -177,7 +177,7 @@ class TemporalTransformerBlock(nn.Module):
         upcast_attention                   = False,
         cross_frame_attention_mode         = None,
         temporal_position_encoding         = False,
-        temporal_position_encoding_max_len = 24,
+        temporal_position_encoding_max_len = 5000,
     ):
         super().__init__()
 
@@ -231,7 +231,7 @@ class PositionalEncoding(nn.Module):
         self, 
         d_model, 
         dropout = 0., 
-        max_len = 24
+        max_len = 5000
     ):
         super().__init__()
         self.dropout = nn.Dropout(p=dropout)
@@ -253,7 +253,7 @@ class VersatileAttention(Attention):
             attention_mode                     = None,
             cross_frame_attention_mode         = None,
             temporal_position_encoding         = False,
-            temporal_position_encoding_max_len = 32,            
+            temporal_position_encoding_max_len = 5000,            
             *args, **kwargs
         ):
         super().__init__(*args, **kwargs)
